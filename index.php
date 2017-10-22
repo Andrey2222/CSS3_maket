cc054e69776316fd2c2e37e529a22467ec85acc9
John Doe <johndoe@example.com>
2017-10-22 11:35:19 +0300

    change php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="filestyle.css">
+    <link rel="stylesheet" href="stylesheets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-dropdown/2.0.3/jquery.dropdown.css">
+    <link rel="stylesheet" href="css/help.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container-fluid custom-header no-padding">
+			<div class="row header-line">
+				<div class="col-md-1"></div>
+				<div class="col-md-10">
+					<div class="row">
+						<div class="col-md-2">
+							<img src="images/logo.png" height="45px">
+						</div>
+						<div class="col-md-10">
+							<nav>
+								<ul class="list-inline menu">
+									<li><a href="#">Home</a></li>
+									<li><a href="#">About</a></li>
+									<li><a href="#">Portfolio</a></li>
+									<li><a href="#">Service</a></li>
+									<li><a href="#">Team</a></li>
+									<li><a href="#">Block</a></li>
+									<li><a href="#">Contact</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>	
+
+					<div class="row main">
+						<div class="col-md-12 text-center">
+							<p class="h1-custom">EXCELLENT WORDPRESS THEME</p>
+						
+							<div class="divider"></div>
+							<p class="description"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laundantium dolor facere comsequunture minus quod veritatis poluptate laboriosam suscipit voluptabas possimus</p>
+							<button class="btn btn-default">More info</button>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row about">
+				<div class="col-md-12 text-center">
+					<h2 class="h1-custom">ABOUT</h2>
+					<div class="divider divider2"></div>
+					<p class="description"> Lorem ipsum dolor sit amet, consetetur odipisicing edit. Culpa id quidem recesstovbus molesige reprehenderitquam</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<h3 class="text-center">DESIGN</h3>
+					<p class="description text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laundantium dolor facere comsequunture minus quod veritatis poluptate laboriosam suscipit voluptabas possimus</p>
+				</div>
+				<div class="col-md-3">
+					<h3 class="text-center">CODING</h3>
+					<p class="description text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laundantium dolor facere comsequunture minus quod veritatis poluptate laboriosam suscipit voluptabas possimus</p>
+				</div>
+				<div class="col-md-3">
+					<h3 class="text-center">DESIGN</h3>
+					<p class="description text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laundantium dolor facere comsequunture minus quod veritatis poluptate laboriosam suscipit voluptabas possimus</p>
+				</div>
+				<div class="col-md-3">
+					<h3 class="text-center">DESIGN</h3>
+					<p class="description text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit.Laundantium dolor facere comsequunture minus quod veritatis poluptate laboriosam suscipit voluptabas possimus</p>
+				</div>
+			</div>
+		</div>	
+	</header>
+	<main>
+		
+	</main>
+
+	 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-dropdown/2.0.3/jquery.dropdown.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+</body>
+</html>
